2c06c62cc7442b15c94e9354fc0021a04bf70a99
Capsizer <Opcelon@gmail.com>
2025-11-06 18:09:02 +0330

    Update vite.config.js

--- proguard-site/vite.config.js
+++ proguard-site/vite.config.js
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // این خط به Vite می‌گوید که آدرس نهایی سایت شما
-  // در یک زیرپوشه به نام /proguard-site/ خواهد بود.
-  base: "/proguard-site/", 
+  // نقشه راه ۱: به Vite می‌گوییم ریشه سایت شما کجاست (جایی که index.html قرار دارد)
+  root: 'src',
+
+  // نقشه راه ۲: آدرس نهایی سایت در گیت‌هاب
+  base: '/proguard-site/',
+
+  build: {
+    // نقشه راه ۳: به Vite می‌گوییم پوشه build شده (dist) را کجا قرار دهد
+    // (یک پوشه بالاتر از ریشه، یعنی در proguard-site/dist)
+    outDir: '../dist',
+    emptyOutDir: true, // همیشه قبل از بیلد، پوشه dist را خالی کن
+  },
+
   plugins: [react()],
 })
